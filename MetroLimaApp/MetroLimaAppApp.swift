e7a2e3a5d6bbf3d9d6a0c32e25d8393a24a16c73
SHEILA-DIAZ <sheila.diaz@tecsup.edu.pe>
2026-09-09 09:30:54 -0500

    Initial commit - Proyecto MetroLimaApp

---
+++ MetroLimaApp/MetroLimaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetroLimaAppApp.swift
+//  MetroLimaApp
+//
+//  Created by Tecsup on 9/09/26.
+//
+
+import SwiftUI
+
+@main
+struct MetroLimaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
